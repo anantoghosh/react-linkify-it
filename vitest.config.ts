84935e5bc29a4e35a352bfe5466eb88b3d83c9a4
Ananto Ghosh <a0ghosh@gmail.com>
2025-09-10 04:54:20 +0530

    build: ignore unnecessary files for coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vitest/config';
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
-    setupFiles: ['./vitest.setup.ts'],
-    include: ['src/**/*.test.tsx'],
+    setupFiles: ["./vitest.setup.ts"],
+    include: ["src/**/*.test.tsx"],
     sequence: {
-      concurrent: false
+      concurrent: false,
+    },
+    coverage: {
+      exclude: [
+        "gpr-hack.js",
+        "rollup-legacy.config.js",
+        ...coverageConfigDefaults.exclude,
+      ],
     },
   },
 });
