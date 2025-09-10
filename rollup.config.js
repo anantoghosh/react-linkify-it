711c6ce81e5489dadce7b609a700994d474f6170
Ananto Ghosh <a0ghosh@gmail.com>
2025-09-11 04:49:33 +0530

    ci: add debug

--- rollup.config.js
+++ rollup.config.js
@@ -24,6 +24,8 @@ export default {
       enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
       bundleName: "react-linkify-it",
       uploadToken: process.env.CODECOV_TOKEN,
+      telemetry: false,
+      debug: true
     }),
   ],
   external: ["react"],
