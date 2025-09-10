504d3c98bebbd5684cb3a130e2d7fa9942426b63
Ananto Ghosh <a0ghosh@gmail.com>
2025-09-11 04:13:59 +0530

    fix(urlRegex): remove unnecessary escape character

--- src/url.tsx
+++ src/url.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { LinkProps } from "types";
 
 export const urlRegex =
-  /(https?:\/\/|www\.)[^\s<>'"()\[\]{}]+[^\s<>'"()\[\]{}.,;:!?\])}]/u;
+  /(https?:\/\/|www\.)[^\s<>'"()[\]{}]+[^\s<>'"()[\]{}.,;:!?\])}]/u;
 
 export const UrlComponent: React.FC<React.PropsWithChildren<LinkProps>> = ({
   match: url,
